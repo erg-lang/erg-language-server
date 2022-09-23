9eeb3a0c116f828fa92619011c549d5f6661c57c
Shunsuke Shibayama <sbym1346@gmail.com>
2022-09-24 03:19:49 +0900

    Add diagnostics & basic completion

--- src/server.rs
+++ src/server.rs
@@ -1,20 +1,48 @@
-use std::io::{StdoutLock, StdinLock, BufRead, Read};
-// use std::path;
-// use std::fs;
 use std::io;
-use std::io::{stdin, stdout, Write, /*BufRead as _*/};
+use std::io::{stdin, stdout, Write, StdoutLock, StdinLock, BufRead, Read};
+use std::path::PathBuf;
 use std::str::FromStr;
+use std::fs::File;
 
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
 use serde_json::{Value};
 use serde_json::json;
+
+use erg_common::color::{RED, YELLOW, RESET};
+use erg_common::config::{ErgConfig, Input};
+use erg_common::traits::{Runnable, Stream};
+
+use erg_type::Type;
+
+use erg_compiler::AccessKind;
+use erg_compiler::context::Context;
+use erg_compiler::build_hir::HIRBuilder;
+
+use lsp_types::{
+    Diagnostic, DiagnosticSeverity, Position, Range, CompletionItem, CompletionItemKind, InitializeResult, ServerCapabilities,
+    TextDocumentSyncCapability, TextDocumentSyncKind, CompletionOptions,
+};
+
+use url::Url;
 
 use crate::message::{LogMessage, ErrorMessage};
 
 type ELSResult<T> = Result<T, Box<dyn std::error::Error>>;
 
+fn get_path_from_uri(uri: &str) -> ELSResult<String> {
+    let url = Url::parse(uri)?;
+    let path = urlencoding::decode(url.path())?.to_string();
+    Ok(path[1..].to_string())
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct ClientCapabilities {
+    pub publish_diagnostics: bool,
+}
+
 pub struct Server {
-    _cfg: String,
+    client_capas: ClientCapabilities,
+    context: Option<Context>,
     input: StdinLock<'static>,
     output: StdoutLock<'static>,
 }
@@ -24,14 +52,14 @@ impl Server {
         let input = stdin().lock();
         let output = stdout().lock();
         Self {
-            _cfg: String::new(),
+            client_capas: ClientCapabilities::default(),
+            context: None,
             input,
             output,
         }
     }
 
     pub fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        self.init()?;
         loop {
             let msg = Value::from_str(&self.read_message()?)?;
             self.dispatch(msg)?;
@@ -58,9 +86,45 @@ impl Server {
         self.send_error(None, -32601, "received an invalid request")
     }
 
-    /// initialize ELS
-    fn init(&mut self) -> ELSResult<()> {
-        self.send_log("initializing ELS")
+    fn send_diagnostics(&mut self, uri: &str, diagnostics: Vec<Diagnostic>) -> ELSResult<()> {
+        if self.client_capas.publish_diagnostics {
+            self.send(&json!({
+                "jsonrpc": "2.0",
+                "method": "textDocument/publishDiagnostics",
+                "params": {
+                    "uri": uri,
+                    "diagnostics": diagnostics,
+                }
+            }))?;
+        } else {
+            self.send_log("client does not support diagnostics")?;
+        }
+        Ok(())
+    }
+
+    #[allow(clippy::field_reassign_with_default)]
+    fn init(&mut self, msg: &Value, id: i64) -> ELSResult<()> {
+        self.send_log("initializing ELS")?;
+        #[allow(clippy::collapsible_if)]
+        if msg.get("params").is_some() && msg["params"].get("capabilities").is_some() {
+            if msg["params"]["capabilities"].get("textDocument").is_some() && msg["params"]["capabilities"]["textDocument"].get("publishDiagnostics").is_some() {
+                // TODO: more detailed configuration
+                self.client_capas.publish_diagnostics = true;
+            }
+            // and other capabilities
+        }
+        // self.send_log(format!("set client capabilities: {:?}", self.client_capas))?;
+        let mut result = InitializeResult::default();
+        result.capabilities = ServerCapabilities::default();
+        result.capabilities.text_document_sync = Some(TextDocumentSyncCapability::from(TextDocumentSyncKind::FULL));
+        let mut comp_options = CompletionOptions::default();
+        comp_options.trigger_characters = Some(vec![".".to_string(), "::".to_string()]);
+        result.capabilities.completion_provider = Some(comp_options);
+        self.send(&json!({
+            "jsonrpc": "2.0",
+            "id": id,
+            "result": result,
+        }))
     }
 
     /// Copied and modified from RLS, https://github.com/rust-lang/rls/blob/master/rls/src/server/io.rs
@@ -142,16 +206,10 @@ impl Server {
         }
     }
 
-    fn handle_method(&mut self, _msg: &Value, id: i64, method: &str) -> ELSResult<()> {
+    fn handle_method(&mut self, msg: &Value, id: i64, method: &str) -> ELSResult<()> {
         match method {
-            "initialize" => {
-                self.send_log("initialize")?;
-                self.send(&json!({
-                    "jsonrpc": "2.0",
-                    "id": id,
-                    "result": { "capabilities": { "textDocumentSync": 1 } }
-                }))
-            },
+            "initialize" => self.init(msg, id),
+            "textDocument/completion" => self.show_completion(msg),
             other => {
                 self.send_error(Some(id), -32600, format!("{other} is not supported"))
             }
@@ -161,9 +219,98 @@ impl Server {
     fn handle_notification(&mut self, msg: &Value, notification: &str) -> ELSResult<()> {
         match notification {
             "initialized" => self.send_log("successfully bound"),
-            "textDocument/didOpen" => self.send_log(format!("Open: {}", msg["params"]["textDocument"]["uri"])),
-            "textDocument/didChange" => self.send_log(format!("Change: {}", msg["params"]["textDocument"]["uri"])),
+            "textDocument/didOpen" => {
+                let uri = msg["params"]["textDocument"]["uri"].as_str().unwrap();
+                let path = get_path_from_uri(uri)?;
+                self.send_log(format!("{notification}: {uri}"))?;
+                self.check_file(
+                    uri,
+                    &path,
+                    msg["params"]["textDocument"]["text"].as_str().unwrap(),
+                )
+            },
+            "textDocument/didSave" => {
+                let uri = msg["params"]["textDocument"]["uri"].as_str().unwrap();
+                let path = get_path_from_uri(uri)?;
+                let mut code = String::new();
+                self.send_log(format!("{notification}: {path}"))?;
+                File::open(&path)?.read_to_string(&mut code)?;
+                self.check_file(uri, &path, &code)
+            },
+            // "textDocument/didChange"
             _ => self.send_log(format!("received notification: {}", notification)),
         }
+    }
+
+    fn check_file<S: Into<String>>(&mut self, uri: &str, path: &str, code: S) -> ELSResult<()> {
+        self.send_log(format!("checking {path}"))?;
+        let cfg = ErgConfig {
+            input: Input::File(PathBuf::from(path)),
+            mode: "exec",
+            opt_level: 1,
+            dump_as_pyc: false,
+            python_ver: None,
+            py_server_timeout: 10,
+            quiet_startup: false,
+            module: "<module>",
+            verbose: 2,
+            ps1: ">>> ",
+            ps2: "... ",
+        };
+        let mut hir_builder = HIRBuilder::new(cfg);
+        match hir_builder.build(code.into(), "exec") {
+            Err(errs) => {
+                self.send_log(format!("found errors: {}", errs.len()))?;
+                let diags = errs.into_iter().map(|err| {
+                    let message = err.core.desc.to_string().replace(RED, "").replace(YELLOW, "").replace(RESET, "");
+                    let start = Position::new(err.core.loc.ln_begin().unwrap() as u32 - 1, err.core.loc.col_begin().unwrap() as u32);
+                    let end = Position::new(err.core.loc.ln_end().unwrap() as u32 - 1, err.core.loc.col_end().unwrap() as u32);
+                    let err_code = err.core.kind as u8;
+                    let severity = if (60..=100).contains(&err_code) || (180..=200).contains(&err_code) {
+                        DiagnosticSeverity::WARNING
+                    } else {
+                        DiagnosticSeverity::ERROR
+                    };
+                    Diagnostic::new(Range::new(start, end), Some(severity), None, None, message, None, None)
+                }).collect();
+                self.send_diagnostics(uri, diags)?;
+            }
+            Ok(_) => {
+                // self.hir = Some(hir);
+                self.send_log(format!("checking {path} passed"))?;
+                self.send_diagnostics(uri, vec![])?;
+            }
+        }
+        self.context = Some(hir_builder.pop_ctx());
+        Ok(())
+    }
+
+    fn show_completion(&mut self, msg: &Value) -> ELSResult<()> {
+        self.send_log(format!("showing completion: {msg}"))?;
+        let trigger = msg["params"]["context"]["triggerCharacter"].as_str();
+        let acc = match trigger {
+            Some(".")| Some("::") => AccessKind::Attr,
+            _ => AccessKind::Name,
+        };
+        self.send_log(format!("AccessKind: {acc:?}"))?;
+        let mut result = vec![];
+        let context = if acc.is_local() { self.context.as_ref().unwrap() } else {
+            // TODO: get context
+            self.send(&json!({ "jsonrpc": "2.0", "id": msg["id"].as_i64().unwrap(), "result": result }))?;
+            return Ok(());
+        };
+        for (name, vi) in context.dir().into_iter() {
+            let mut item = CompletionItem::new_simple(name.to_string(), vi.t.to_string());
+            item.kind = match &vi.t {
+                Type::Subr(_) => Some(CompletionItemKind::FUNCTION),
+                Type::Module => Some(CompletionItemKind::MODULE),
+                Type::Class | Type::Trait => Some(CompletionItemKind::CLASS),
+                _ if vi.muty.is_const() => Some(CompletionItemKind::CONSTANT),
+                _ => Some(CompletionItemKind::VARIABLE),
+            };
+            result.push(item);
+        }
+        // CompletionTriggerKind
+        self.send(&json!({ "jsonrpc": "2.0", "id": msg["id"].as_i64().unwrap(), "result": result }))
     }
 }
